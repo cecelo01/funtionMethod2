a4147cc1537539521ce842698a7c19c81eff4cd5
Marcelo Vieira <48066643+cecelo01@users.noreply.github.com>
2022-06-07 17:46:55 -0300

    up

---
+++ src/br/funtionmethod2/Operations.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.funtionmethod2;
+
+/**
+ *
+ * @author Marce
+ */
+public class Operations {
+    
+    public static String Contador( int i, int f){
+        String s = "";
+        for (int c=0; c <= f; c++){
+            s += c + " ";
+        }
+        return s;
+    }
+    
+    
+}
